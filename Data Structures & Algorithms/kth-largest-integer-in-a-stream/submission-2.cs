a10d5ffc83811a87c3c33e75ee963e60a1b906f2
Vibush <vibushks@gmail.com>
2026-08-31 17:15:17 -0500

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.cs
@@ -0,0 +1,27 @@
+public class KthLargest {
+    PriorityQueue<int, int> maxHeap;
+    int _k;
+
+    public KthLargest(int k, int[] nums) {
+        _k = k;
+        maxHeap = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            maxHeap.Enqueue(nums[i], nums[i]);
+
+            if (maxHeap.Count > k) {
+                maxHeap.Dequeue();
+            }
+        }
+    }
+    
+    public int Add(int val) {
+        maxHeap.Enqueue(val, val);
+
+        if (maxHeap.Count > _k) {
+            maxHeap.Dequeue();
+        }
+
+        return maxHeap.Peek();
+    }
+}
